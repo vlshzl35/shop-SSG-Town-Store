825e0fc93b91052c5ef8a26224ed52032cba5d2a
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-06-24 16:24:30 +0900

    Feat: RefundQueryService 추가  #12

--- admin/src/main/java/com/sh/admin/refund/model/service/RefundQueryService.java
+++ admin/src/main/java/com/sh/admin/refund/model/service/RefundQueryService.java
@@ -1,4 +1,21 @@
-package com.sh.admin.refund.model.dto;
+package com.sh.admin.refund.model.service;
 
-public class RefundService {
+import com.sh.admin.refund.model.dao.RefundMapper;
+import com.sh.admin.refund.model.dto.RefundDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class RefundQueryService {
+    private final RefundMapper refundMapper;
+
+    @Transactional
+    public List<RefundDto> findAll() {
+        return refundMapper.findAll();
+    }
 }
