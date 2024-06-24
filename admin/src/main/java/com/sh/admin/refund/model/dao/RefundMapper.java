e579da98f4971c5d6c9aa7eac25811c37b058f7e
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-06-24 16:25:32 +0900

    Feat: Refund findAll 기능 추가 #12

--- admin/src/main/java/com/sh/admin/refund/model/dao/RefundMapper.java
+++ admin/src/main/java/com/sh/admin/refund/model/dao/RefundMapper.java
@@ -1,4 +1,11 @@
 package com.sh.admin.refund.model.dao;
 
+import com.sh.admin.refund.model.dto.RefundDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface RefundMapper {
+    List<RefundDto> findAll();
 }
