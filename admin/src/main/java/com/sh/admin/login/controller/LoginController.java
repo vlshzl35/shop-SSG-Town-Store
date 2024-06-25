1406b9aa30b0fc68f3c6215deeff0266b9772b77
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-06-25 14:50:37 +0900

    로그인 기능 완성 #20
    test

---
+++ admin/src/main/java/com/sh/admin/login/controller/LoginController.java
@@ -0,0 +1,57 @@
+package com.sh.admin.login.controller;
+
+
+import com.sh.admin.login.model.dto.LoginRequestDto;
+import com.sh.admin.login.model.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.support.SessionStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/admin")
+@SessionAttributes("userId")
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login/login";
+    }
+
+    @PostMapping("/loginExecute")
+    public ResponseEntity<?> loginExecute(@RequestParam String adminId, @RequestParam String adminPassword) {
+        LoginRequestDto loginRequestDto = loginService.login(adminId, adminPassword);
+        Map<String, String> response = new HashMap<>();
+
+        if (loginRequestDto != null) {
+            response.put("status", "200");
+            response.put("message", "Login successful");
+            response.put("adminName",loginRequestDto.getAdminName());
+            response.put("adminId", loginRequestDto.getAdminId());
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("status", "401");
+            response.put("message", "Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(SessionStatus sessionStatus) {
+
+        sessionStatus.setComplete(); // 세션 폐기
+        return "redirect:/login/login";
+    }
+}
--- admin/src/main/java/com/sh/admin/login/model/service/LoginService.java
+++ admin/src/main/java/com/sh/admin/login/model/service/LoginService.java
@@ -1,9 +1,20 @@
 package com.sh.admin.login.model.service;
 
-import lombok.extern.slf4j.Slf4j;
+import com.sh.admin.login.model.dao.LoginMapper;
+import com.sh.admin.login.model.dto.LoginRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LoginService {
+    private final LoginMapper loginMapper;
+
+    public LoginRequestDto login(String adminId, String adminPassword) {
+        System.out.println(adminId + " "  +adminPassword + "mapper");
+        return loginMapper.login(adminId, adminPassword);
+
+    }
 }
