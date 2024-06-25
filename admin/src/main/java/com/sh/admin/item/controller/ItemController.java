858ef472be534839c2cf324e9cdc0324d87cf878
kishi <81032378+vlshzl35@users.noreply.github.com>
2024-06-25 14:12:44 +0900

    Feat: 다중 조건 검색 ~ing #8

--- admin/src/main/java/com/sh/admin/item/controller/ItemController.java
+++ admin/src/main/java/com/sh/admin/item/controller/ItemController.java
@@ -1,16 +1,23 @@
 package com.sh.admin.item.controller;
 
+import com.sh.admin.item.model.dto.Artist;
+import com.sh.admin.item.model.dto.Category;
 import com.sh.admin.item.model.dto.ItemDto;
+import com.sh.admin.item.model.dto.SaleStatus;
 import com.sh.admin.item.model.service.ItemCommandService;
 import com.sh.admin.item.model.service.ItemQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
+import static com.sh.admin.item.model.dto.Category.CD;
 
 @Controller
 @Slf4j
@@ -30,6 +37,27 @@ public class ItemController {
         model.addAttribute("items", items);
         return "item/list";
     }
+
+    /* 희윤 시작 */
+    @PostMapping("/list") // 다중검색 조건 설정값 넘겨받기
+    public String regist(
+                @RequestParam(required = false) Category categoryName,
+                @RequestParam(required = false) Artist artistName,
+                @RequestParam(required = false) SaleStatus saleStatus,
+                @RequestParam(required = false) int salePrice,
+                Model model){
+                log.info("POST /list");
+
+                // 검색 결과 가져오기
+                List<ItemDto> items = itemQueryService.findAllMatch(categoryName, artistName, salePrice, saleStatus);
+                log.debug("items = {}", items);
+                // 모델에 검색 결과 추가
+                model.addAttribute("items", items);
+
+                // 결과 반환
+                return "item/list";
+    }
+    /* 희윤 끝 */
 
     @GetMapping("/create") // 상품등록 페이지
     public String create() {
---
+++ admin/src/test/java/com/sh/admin/item/model/dao/ItemMapperTest.java
@@ -0,0 +1,41 @@
+package com.sh.admin.item.model.dao;
+
+import com.sh.admin.item.model.dto.ItemDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemMapperTest {
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+
+    @Test
+    @DisplayName("전체조회")
+    void findAll() {
+        // given // 조회라서 줘야되는게 없음
+        // when
+        List<ItemDto> items = itemMapper.findAll();
+        System.out.println(items);
+
+        //then
+        assertThat(items)
+                .isNotNull()
+                .isNotEmpty()
+                .allMatch((item) -> item != null); // 모든 요소가 다 true
+
+    }
+
+    @Test
+    void findAllMatch() {
+        
+    }
+}
