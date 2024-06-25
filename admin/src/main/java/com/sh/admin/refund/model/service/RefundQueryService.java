ff8a93e22c0d3ba5d557944e89293d02712090e0
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-06-25 14:54:58 +0900

    Feat: 환불 승인 기능 추가, DB 테스트 및 백-프론트 연결 #14

--- admin/src/main/java/com/sh/admin/refund/model/service/RefundQueryService.java
+++ admin/src/main/java/com/sh/admin/refund/model/service/RefundQueryService.java
@@ -2,6 +2,8 @@ package com.sh.admin.refund.model.service;
 
 import com.sh.admin.refund.model.dao.RefundMapper;
 import com.sh.admin.refund.model.dto.RefundDto;
+import com.sh.admin.refund.model.dto.RefundItemDto;
+import com.sh.admin.refund.model.dto.RefundOrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +19,13 @@ public class RefundQueryService {
     @Transactional
     public List<RefundDto> findAll() {
         return refundMapper.findAll();
+    }
+
+    public RefundOrderDto findOrder(long orderId) {
+        return refundMapper.findOrder(orderId);
+    }
+
+    public List<RefundItemDto> findOrderItem(long orderId) {
+        return refundMapper.findOrderItem(orderId);
     }
 }
