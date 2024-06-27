6b342c486ae325ef22f94ee383e42bc92aa49f39
kishi <81032378+vlshzl35@users.noreply.github.com>
2024-06-27 11:57:08 +0900

    Feat: 상품 등록(base64를 이용한 이미지 업로드) #9

--- admin/src/main/java/com/sh/admin/item/model/dto/ItemRegistDto.java
+++ admin/src/main/java/com/sh/admin/item/model/dto/ItemRegistDto.java
@@ -1,19 +1,24 @@
 package com.sh.admin.item.model.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-public class ItemDto {
-    private Long itemId; // 상품ID
+public class ItemRegistDto {
     private String itemName; // 상품명
     private Category categoryName; // 카테고리
     private Artist artistName; // 아티스트
     private String imgUrl; // 이미지
-    private String seller; // 판매원
     private String details; // 세부설명
     private int quantity;  // 수량
     private int salePrice; // 가격
     private SaleStatus saleStatus; // 판매상태
+
+    public ItemDto toItemDto() {
+        return new ItemDto(null,this.itemName,this.categoryName,this.artistName,this.imgUrl,null,this.details,this.quantity,this.salePrice,this.saleStatus);
+    }
+
 }
